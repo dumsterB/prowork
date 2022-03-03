3b23df8d3575c0106d4ab1d17d0519448cebe84b
Bexruz <74446521+dumsterB@users.noreply.github.com>
2022-03-03 21:44:26 +0500

    initiall commit

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,64 +1,5 @@
 <template>
   <div>
-    <a-layout-header class="header" style="background:  rgb(0, 33, 64);">
-      <div style="width: 256px">
-        <a-button type="primary" style="margin-bottom: 16px" @click="toggleCollapsed">
-          <a-icon :type="collapsed ? 'menu-unfold' : 'menu-fold'"/>
-        </a-button>
-        <a-menu
-            :default-selected-keys="['1']"
-            :default-open-keys="['sub1']"
-            mode="inline"
-            theme="dark"
-            :inline-collapsed="collapsed"
-        >
-          <a-menu-item key="1">
-            <a-icon type="pie-chart"/>
-            <span>Option 1</span>
-          </a-menu-item>
-          <a-menu-item key="2">
-            <a-icon type="desktop"/>
-            <span>Option 2</span>
-          </a-menu-item>
-          <a-menu-item key="3">
-            <a-icon type="inbox"/>
-            <span>Option 3</span>
-          </a-menu-item>
-          <a-sub-menu key="sub1">
-            <span ><a-icon type="mail"/><span>Navigation One</span></span>
-            <a-menu-item key="5">
-              Option 5
-            </a-menu-item>
-            <a-menu-item key="6">
-              Option 6
-            </a-menu-item>
-            <a-menu-item key="7">
-              Option 7
-            </a-menu-item>
-            <a-menu-item key="8">
-              Option 8
-            </a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <span ><a-icon type="appstore"/><span>Navigation Two</span></span>
-            <a-menu-item key="9">
-              Option 9
-            </a-menu-item>
-            <a-menu-item key="10">
-              Option 10
-            </a-menu-item>
-            <a-sub-menu key="sub3" title="Submenu">
-              <a-menu-item key="11">
-                Option 11
-              </a-menu-item>
-              <a-menu-item key="12">
-                Option 12
-              </a-menu-item>
-            </a-sub-menu>
-          </a-sub-menu>
-        </a-menu>
-      </div>
-    </a-layout-header>
     <router-view></router-view>
   </div>
 </template>
@@ -81,5 +22,7 @@ export default defineComponent({
 </script>
 
 <style scoped>
-
+.ant-layout-header{
+  padding: 0px 5px!important;
+}
 </style>
